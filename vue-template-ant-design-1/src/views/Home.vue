27a8b563e598c26dd3b8b2fc3381ba21ff71942f
ataola <zjt613@gmail.com>
2020-10-29 22:26:22 +0800

    vue cli with ts

---
+++ vue-template-ant-design-1/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p>{{ title }}</p>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      title: process.env.VUE_APP_TITLE,
+    };
+  },
+};
+</script>
---
+++ vue-template-ant-design-1/vue.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const fs = require('fs');
+const { env } = process;
+const is_production = env.NODE_ENV === 'production';
+
+function addStyleResource(rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/styles/abstracts/*.styl'),
+      ],
+    });
+}
+
+module.exports = {
+  publicPath: env.PUBLIC_PATH,
+  outputDir: 'dist',
+  indexPath: 'public/index.html',
+  chainWebpack: config => {
+    // auto imports abstract styles to each vue source file
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)));
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src'),
+      }
+    },
+  },
+  css: {
+    requireModuleExtension: true,
+    loaderOptions: {
+      css: {
+        // 注意：以下配置在 Vue CLI v4 与 v3 之间存在差异。
+        // Vue CLI v3 用户可参考 css-loader v1 文档
+        // https://github.com/webpack-contrib/css-loader/tree/v1.0.1
+        modules: {
+          localIdentName: '[name]-[local]_[hash:base64:5]'
+        },
+        // less: {
+        //   javascriptEnabled: true
+        // },
+        localsConvention: 'camelCaseOnly'
+      }
+    }
+  },
+  devServer: {
+    open: true,
+    https: false,
+    port: 9000,
+    disableHostCheck: true,
+  },
+}
