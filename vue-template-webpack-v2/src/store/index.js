4b5de4bfbd506aa2cae4e8d8e124db02a767848d
ataola <zjt613@gmail.com>
2020-11-16 21:11:57 +0800

    add global state

--- vue-template-webpack-v2/src/store/index.js
+++ vue-template-webpack-v2/src/store/index.js
@@ -1,12 +1,85 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import {
+  IS_LOGIN,
+  IS_REGISTER,
+  IS_LOADING,
+  IS_SEARCH,
+  IS_FOOTER,
+  IS_BACK,
+  IS_SHARE,
+  LOGIN,
+  LOUOUT,
+  REGISTER
+} from './mutation-types';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    // 相关的判断状态
+    judge: {
+      // 是否登录
+      is_login: true,
+      // 是否注册
+      is_register: false,
+      // 是否加载
+      is_loading: false,
+      // 是否显示搜索
+      is_search: true,
+      // 是否显示底部
+      is_footer: true,
+      // 是否显示返回按钮
+      is_back: false,
+      // 是否显示分享按钮
+      is_share: false
+    },
+    // 相关的用户信息
+    userinfo: {
+      name: 'guest',
+      password: 'guest',
+      nickname: 'guest',
+      avatar: ''
+    },
+    // 相关的页面配置
+    page: {
+      title: ''
+    }
+    // 相关的权限控制
+  },
   getters: {},
-  mutations: {},
+  mutations: {
+    [IS_LOGIN](state) {
+      return state.judge.is_login;
+    },
+    [IS_REGISTER](state) {
+      return state.judge.is_register;
+    },
+    [IS_LOADING](state) {
+      return state.judge.is_loading;
+    },
+    [IS_SEARCH](state) {
+      return state.judge.is_search;
+    },
+    [IS_FOOTER](state) {
+      return state.judge.is_footer;
+    },
+    [IS_BACK](state) {
+      return state.judge.is_back;
+    },
+    [IS_SHARE](state) {
+      return state.judge.is_share;
+    },
+    [LOGIN](state) {
+      // 思考中
+    },
+    [LOUOUT](state) {
+      // 思考中
+    },
+    [REGISTER](state) {
+      // 思考中
+    }
+  },
   actions: {},
   modules: {}
 });
---
+++ vue-template-webpack-v2/src/store/mutation-types.js
@@ -0,0 +1,13 @@
+// 判断相关状态
+export const IS_LOGIN = 'isLogin';
+export const IS_REGISTER = 'isRegister';
+export const IS_LOADING = 'isLoading';
+export const IS_SEARCH = 'isSearch';
+export const IS_FOOTER = 'isFooter';
+export const IS_BACK = 'isBack';
+export const IS_SHARE = 'isShare';
+
+// 用户相关操作
+export const LOGIN = 'login';
+export const LOUOUT = 'logout';
+export const REGISTER = 'register';
