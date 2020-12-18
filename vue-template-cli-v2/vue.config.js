b1dfc4550a458c8c72155a246fdbd85ec2c2feb9
ataola <zjt613@gmail.com>
2020-12-18 16:28:47 +0800

    add alias

--- vue-template-cli-v2/vue.config.js
+++ vue-template-cli-v2/vue.config.js
@@ -1,7 +1,21 @@
 const port = process.env.port || process.env.npm_config_port || 8090;
 const is_prod = process.env.NODE_ENV === 'production';
+const resolve = dir => path.join(__dirname, dir);
 module.exports = {
   lintOnSave: !is_prod,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', 'src')
+      .set('@assets', resolve('src/assets'))
+      .set('@scss', resolve('src/assets/scss'))
+      .set('@components', resolve('src/components'))
+      .set('@plugins', resolve('src/plugins'))
+      .set('@views', resolve('src/views'))
+      .set('@router', resolve('src/router'))
+      .set('@store', resolve('src/store'))
+      .set('@layouts', resolve('src/layouts'))
+      .set('@static', resolve('src/static'));
+  },
   devServer: {
     open: true,
     host: '0.0.0.0',
